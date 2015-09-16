888c774b9a0f5bf64f26eac64532e6e1647ce01e
Jeffrey Dare <jeffdare@users.noreply.github.com>
2015-09-16 22:45:55 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,39 +1,23 @@
 #! /bin/sh
 
 
-#varibles
-CONFIG_FILE=./config
-
-check_and_install () {
-	for word in "$@"
-	do
-		npm list $word > /dev/null 2>&1
-		[ $? -ne 0 ] && npm install $word
-	done
-	
-}
-
 error_exit(){
 	echo "$1"
 	exit 1
 	}
 
-
-check_and_install mqtt coap node-tts-api
+npm install
 
 echo "Enter Game ID :  "
 read GAME_ID
-
 
 echo ""
 echo "Entered game ID is "${GAME_ID}""
 echo ""
 
-#sed 's/.*gameId.*/\"gameid\":\"'${GAME_ID}'\"/g' ${CONFIG_FILE} 
+echo '{"gameid":"'${GAME_ID}'"}' > game.json
 
-echo '{"gameid":"'${GAME_ID}'"}' > gameid.json
-
-cat gameid.json
+cat game.json
 
 sudo chmod 0777 /home/pi/RRP/rrpd 
 [ $? -ne 0 ] && error_exit "error: rrpd file permission was not successful"
@@ -44,5 +28,3 @@ sudo cp -f /home/pi/RRP/rrpd /etc/init.d/
 sudo update-rc.d rrpd defaults
 
 sudo service rrpd start
-
-
